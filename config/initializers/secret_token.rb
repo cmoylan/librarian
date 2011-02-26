cfbfb1a56d2062469966d44b6a55bedd617c628b
cmoylan <chris@chrismoylan.com>
2011-02-26 10:48:36 -0600

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Librarian::Application.config.secret_token = '119a64f7ee7ca61dd4ce19efaff2e72ddb1c340b467e0227ed123d0816bba5c838a424c02fd94ea981b7c18a6768f27bc427382478e66942da4af6c31af0cf4e'
