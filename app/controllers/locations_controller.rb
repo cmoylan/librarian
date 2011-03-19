75dbd88fc74a88ac6319ef3442e3e741dc796db0
cmoylan <chris@chrismoylan.com>
2011-03-19 15:34:56 -0500

    added locations and carrierwave for file uploads

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,35 +1,23 @@
 class LocationsController < ApplicationController
   # GET /locations
-  # GET /locations.xml
   def index
     @locations = Location.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @locations }
-    end
   end
 
   # GET /locations/1
   # GET /locations/1.xml
-  def show
-    @location = Location.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @location }
-    end
-  end
+  #def show
+  #  @location = Location.find(params[:id])
+  #
+  #  respond_to do |format|
+  #    format.html # show.html.erb
+  #    format.xml  { render :xml => @location }
+  #  end
+  #end
 
   # GET /locations/new
-  # GET /locations/new.xml
   def new
     @location = Location.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @location }
-    end
   end
 
   # GET /locations/1/edit
@@ -38,46 +26,32 @@ class LocationsController < ApplicationController
   end
 
   # POST /locations
-  # POST /locations.xml
   def create
     @location = Location.new(params[:location])
 
-    respond_to do |format|
-      if @location.save
-        format.html { redirect_to(@location, :notice => 'Location was successfully created.') }
-        format.xml  { render :xml => @location, :status => :created, :location => @location }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
-      end
+    if @location.save
+      redirect_to locations_path, :notice => 'Location was successfully created.'
+    else
+      render :action => "new"
     end
   end
 
   # PUT /locations/1
-  # PUT /locations/1.xml
   def update
     @location = Location.find(params[:id])
 
-    respond_to do |format|
-      if @location.update_attributes(params[:location])
-        format.html { redirect_to(@location, :notice => 'Location was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
-      end
+    if @location.update_attributes(params[:location])
+      redirect_to locations_path, :notice => 'Location was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /locations/1
-  # DELETE /locations/1.xml
   def destroy
     @location = Location.find(params[:id])
     @location.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(locations_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to locations_url
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,9 @@
 class Location < ActiveRecord::Base
+  has_many :bookshelves
+
+  validates_presence_of :name
+  validates_presence_of :map
+
+  mount_uploader :map, MapUploader
+
 end
