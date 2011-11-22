886aa8eda8d536b6b4f2742a2b542c5bbfa5e6fc
Chris Moylan <chris@chrismoylan.com>
2011-11-21 21:14:41 -0600

    switched filenames around

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,60 +1,60 @@
 require 'spec_helper'
 
-describe BookshelvesController do
+describe BooksController do
 
-  def mock_bookshelf(stubs={})
-    @mock_bookshelf ||= mock_model(Bookshelf, stubs).as_null_object
+  def mock_book(stubs={})
+    @mock_book ||= mock_model(Book, stubs).as_null_object
   end
 
   describe "GET new" do
-    it "assigns a new bookshelf as @bookshelf" do
-      Bookshelf.stub(:new) { mock_bookshelf }
+    it "assigns a new book as @book" do
+      Book.stub(:new) { mock_book }
       get :new
-      assigns(:bookshelf).should be(mock_bookshelf)
+      assigns(:book).should be(mock_book)
     end
   end
 
   describe "POST create" do
     describe "with valid params" do
-      it "assigns a newly created bookshelf as @bookshelf" do
-        Bookshelf.stub(:new).with({'these' => 'params'}) { mock_bookshelf(:save => true) }
-        post :create, :bookshelf => {'these' => 'params'}
-        assigns(:bookshelf).should be(mock_bookshelf)
+      it "assigns a newly created book as @book" do
+        Book.stub(:new).with({'these' => 'params'}) { mock_book(:save => true) }
+        post :create, :book => {'these' => 'params'}
+        assigns(:book).should be(mock_book)
       end
 
-      it "redirects to the created bookshelf" do
-        Bookshelf.stub(:new) { mock_bookshelf(:save => true) }
-        post :create, :bookshelf => {}
-        response.should redirect_to(bookshelf_url(mock_bookshelf))
+      it "redirects to the created book" do
+        Book.stub(:new) { mock_book(:save => true) }
+        post :create, :book => {}
+        response.should redirect_to(book_url(mock_book))
       end
     end
 
     describe "with invalid params" do
-      it "assigns a newly created but unsaved bookshelf as @bookshelf" do
-        Bookshelf.stub(:new).with({'these' => 'params'}) { mock_bookshelf(:save => false) }
-        post :create, :bookshelf => {'these' => 'params'}
-        assigns(:bookshelf).should be(mock_bookshelf)
+      it "assigns a newly created but unsaved book as @book" do
+        Book.stub(:new).with({'these' => 'params'}) { mock_book(:save => false) }
+        post :create, :book => {'these' => 'params'}
+        assigns(:book).should be(mock_book)
       end
 
       it "re-renders the 'new' template" do
-        Bookshelf.stub(:new) { mock_bookshelf(:save => false) }
-        post :create, :bookshelf => {}
+        Book.stub(:new) { mock_book(:save => false) }
+        post :create, :book => {}
         response.should render_template("new")
       end
     end
   end
 
   describe "DELETE destroy" do
-    it "destroys the requested bookshelf" do
-      Bookshelf.stub(:find).with("37") { mock_bookshelf }
-      mock_bookshelf.should_receive(:destroy)
+    it "destroys the requested book" do
+      Book.stub(:find).with("37") { mock_book }
+      mock_book.should_receive(:destroy)
       delete :destroy, :id => "37"
     end
 
-    it "redirects to the bookshelves list" do
-      Bookshelf.stub(:find) { mock_bookshelf }
+    it "redirects to the books list" do
+      Book.stub(:find) { mock_book }
       delete :destroy, :id => "1"
-      response.should redirect_to(bookshelves_url)
+      response.should redirect_to(books_url)
     end
   end
 
--- spec/controllers/bookshelves_controller_spec.rb
+++ spec/controllers/bookshelves_controller_spec.rb
@@ -1,60 +1,60 @@
 require 'spec_helper'
 
-describe BooksController do
+describe BookshelvesController do
 
-  def mock_book(stubs={})
-    @mock_book ||= mock_model(Book, stubs).as_null_object
+  def mock_bookshelf(stubs={})
+    @mock_bookshelf ||= mock_model(Bookshelf, stubs).as_null_object
   end
 
   describe "GET new" do
-    it "assigns a new book as @book" do
-      Book.stub(:new) { mock_book }
+    it "assigns a new bookshelf as @bookshelf" do
+      Bookshelf.stub(:new) { mock_bookshelf }
       get :new
-      assigns(:book).should be(mock_book)
+      assigns(:bookshelf).should be(mock_bookshelf)
     end
   end
 
   describe "POST create" do
     describe "with valid params" do
-      it "assigns a newly created book as @book" do
-        Book.stub(:new).with({'these' => 'params'}) { mock_book(:save => true) }
-        post :create, :book => {'these' => 'params'}
-        assigns(:book).should be(mock_book)
+      it "assigns a newly created bookshelf as @bookshelf" do
+        Bookshelf.stub(:new).with({'these' => 'params'}) { mock_bookshelf(:save => true) }
+        post :create, :bookshelf => {'these' => 'params'}
+        assigns(:bookshelf).should be(mock_bookshelf)
       end
 
-      it "redirects to the created book" do
-        Book.stub(:new) { mock_book(:save => true) }
-        post :create, :book => {}
-        response.should redirect_to(book_url(mock_book))
+      it "redirects to the created bookshelf" do
+        Bookshelf.stub(:new) { mock_bookshelf(:save => true) }
+        post :create, :bookshelf => {}
+        response.should redirect_to(bookshelf_url(mock_bookshelf))
       end
     end
 
     describe "with invalid params" do
-      it "assigns a newly created but unsaved book as @book" do
-        Book.stub(:new).with({'these' => 'params'}) { mock_book(:save => false) }
-        post :create, :book => {'these' => 'params'}
-        assigns(:book).should be(mock_book)
+      it "assigns a newly created but unsaved bookshelf as @bookshelf" do
+        Bookshelf.stub(:new).with({'these' => 'params'}) { mock_bookshelf(:save => false) }
+        post :create, :bookshelf => {'these' => 'params'}
+        assigns(:bookshelf).should be(mock_bookshelf)
       end
 
       it "re-renders the 'new' template" do
-        Book.stub(:new) { mock_book(:save => false) }
-        post :create, :book => {}
+        Bookshelf.stub(:new) { mock_bookshelf(:save => false) }
+        post :create, :bookshelf => {}
         response.should render_template("new")
       end
     end
   end
 
   describe "DELETE destroy" do
-    it "destroys the requested book" do
-      Book.stub(:find).with("37") { mock_book }
-      mock_book.should_receive(:destroy)
+    it "destroys the requested bookshelf" do
+      Bookshelf.stub(:find).with("37") { mock_bookshelf }
+      mock_bookshelf.should_receive(:destroy)
       delete :destroy, :id => "37"
     end
 
-    it "redirects to the books list" do
-      Book.stub(:find) { mock_book }
+    it "redirects to the bookshelves list" do
+      Bookshelf.stub(:find) { mock_bookshelf }
       delete :destroy, :id => "1"
-      response.should redirect_to(books_url)
+      response.should redirect_to(bookshelves_url)
     end
   end
 
